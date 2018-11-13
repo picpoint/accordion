ba2040e83f3080d0e07badcd549496433cb03e1c
rmtar <picpoint@yandex.ru>
2018-11-13 23:21:45 +0300

    accordion 13_11_2018

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,24 @@
-function showBlock() {
-	var block = document.getElementById('basedescribe');
+function showBase() {
+	var block = document.getElementById('bd');
+	block.classList.toggle('show');
+}
+
+function showFunc() {
+	var block = document.getElementById('fnc');
+	block.classList.toggle('show');
+}
+
+function showErr() {
+	var block = document.getElementById('err');
+	block.classList.toggle('show');
+}
+
+function showObj() {
+	var block = document.getElementById('obj');
+	block.classList.toggle('show');
+}
+
+function showEvent() {
+	var block = document.getElementById('event');
 	block.classList.toggle('show');
 }
