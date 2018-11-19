d466531eb805dc1b28b5f8a20e97dd0a07b8c586
rmtar <picpoint@yandex.ru>
2018-11-19 23:41:12 +0300

    edit script js

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,47 +1,24 @@
 /*
-function showBase() {
-	var block = document.getElementById('bd');
-	block.classList.toggle('show');
-}
-
-function showFunc() {
-	var block = document.getElementById('fnc');
-	block.classList.toggle('show');
-}
-
-function showErr() {
-	var block = document.getElementById('err');
-	block.classList.toggle('show');
-}
-
-function showObj() {
-	var block = document.getElementById('obj');
-	block.classList.toggle('show');
-}
-
-function showEvent() {
-	var block = document.getElementById('event');
-	block.classList.toggle('show');
-}
-*/
-
-
-var img = document.getElementById('pict');
-var trnglup = document.getElementById('pict');
-var basedesc = document.getElementById('basedesc');
-
+var img = document.querySelector('.triangle');
+var hide = document.querySelector('.hide');
 
 img.addEventListener('click', function () {
-		basedesc.classList.toggle('show');
+	hide.classList.toggle('show');
 
 		if (img.getAttribute('src') === 'pict/triangle_up.png') {
-			return img.setAttribute('src', 'pict/triangle_down.png');
+			img.setAttribute('src', 'pict/triangle_down.png');
+		} else {
+			img.setAttribute('src', 'pict/triangle_up.png');
 		}
-
-		if (img.getAttribute('src') === 'pict/triangle_down.png') {
-			return img.setAttribute('src', 'pict/triangle_up.png');
-		}
-
 });
+*/
 
+var itemContainer = document.querySelector('.wrapper-block-items');
+//var img = document.querySelector('.triangle');
+//var hide = document.querySelector('.hide');
 
+itemContainer.addEventListener('click', function (e) {
+	if (e.target.tagName === 'IMG' && e.target.className === 'triangle') {
+		console.log(e.target + ' this triangle');
+	}
+});
