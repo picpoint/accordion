245f6c638b73c10f1fb7326c01dacf149dcf1093
picpoint <picpoint@yandex.ru>
2018-11-20 16:48:20 +0300

    add events to picture

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,24 +1,20 @@
-/*
-var img = document.querySelector('.triangle');
-var hide = document.querySelector('.hide');
-
-img.addEventListener('click', function () {
-	hide.classList.toggle('show');
-
-		if (img.getAttribute('src') === 'pict/triangle_up.png') {
-			img.setAttribute('src', 'pict/triangle_down.png');
-		} else {
-			img.setAttribute('src', 'pict/triangle_up.png');
-		}
-});
-*/
 
 var itemContainer = document.querySelector('.wrapper-block-items');
-//var img = document.querySelector('.triangle');
-//var hide = document.querySelector('.hide');
+var blockShow = document.querySelector('.hide');
 
 itemContainer.addEventListener('click', function (e) {
 	if (e.target.tagName === 'IMG' && e.target.className === 'triangle') {
-		console.log(e.target + ' this triangle');
+		if (e.target.getAttribute('src') === 'pict/triangle_down.png') {
+			e.target.setAttribute('src', 'pict/triangle_up.png');
+		} else {
+			e.target.setAttribute('src', 'pict/triangle_down.png');
+		}
 	}
+
+
+	if (e.target.tagName === 'DIV' && e.target.className === 'hide') {
+		//blockShow.classList.toggle('show');
+		e.target.classList.toggle('show');
+	}
+
 });
