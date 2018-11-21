b9c7ea7bb22e09351365fd91af2fe58ded118f4b
rmtar <picpoint@yandex.ru>
2018-11-21 22:33:58 +0300

    finaly

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,20 +1,18 @@
-
 var itemContainer = document.querySelector('.wrapper-block-items');
-var blockShow = document.querySelector('.hide');
+var blockShow = document.querySelectorAll('.hide');
 
 itemContainer.addEventListener('click', function (e) {
+
 	if (e.target.tagName === 'IMG' && e.target.className === 'triangle') {
+		var block = e.target.parentElement.parentElement.nextElementSibling;
+
 		if (e.target.getAttribute('src') === 'pict/triangle_down.png') {
 			e.target.setAttribute('src', 'pict/triangle_up.png');
+			block.classList.add('show');
 		} else {
 			e.target.setAttribute('src', 'pict/triangle_down.png');
+			block.classList.remove('show');
 		}
-	}
-
-
-	if (e.target.tagName === 'DIV' && e.target.className === 'hide') {
-		//blockShow.classList.toggle('show');
-		e.target.classList.toggle('show');
 	}
 
 });
