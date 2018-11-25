9363fcdded90ab10a07ca02fd3c9f3590cd6121e
rmtar <picpoint@yandex.ru>
2018-11-25 23:20:51 +0300

    add class to highlight block to orange

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,18 +1,22 @@
 var itemContainer = document.querySelector('.wrapper-block-items');
-var blockShow = document.querySelectorAll('.hide');
 
 itemContainer.addEventListener('click', function (e) {
 
 	if (e.target.tagName === 'IMG' && e.target.className === 'triangle') {
 		var block = e.target.parentElement.parentElement.nextElementSibling;
+		var item = e.target.parentElement.parentElement;
 
 		if (e.target.getAttribute('src') === 'pict/triangle_down.png') {
 			e.target.setAttribute('src', 'pict/triangle_up.png');
 			block.classList.add('show');
+		 item.classList.add('orangebkg');
 		} else {
 			e.target.setAttribute('src', 'pict/triangle_down.png');
 			block.classList.remove('show');
+			item.classList.remove('orangebkg');
 		}
 	}
+
+
 
 });
