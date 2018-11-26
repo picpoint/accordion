15ed87eaa126c95a01732a7f61e594313fd415d6
picpoint <picpoint@yandex.ru>
2018-11-26 16:52:56 +0300

    add click to img's

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,6 @@
 var itemContainer = document.querySelector('.wrapper-block-items');
+var allItemContainer = document.querySelectorAll('.wrapper-block-items-item');
+
 
 itemContainer.addEventListener('click', function (e) {
 
@@ -9,7 +11,7 @@ itemContainer.addEventListener('click', function (e) {
 		if (e.target.getAttribute('src') === 'pict/triangle_down.png') {
 			e.target.setAttribute('src', 'pict/triangle_up.png');
 			block.classList.add('show');
-		 item.classList.add('orangebkg');
+		  item.classList.add('orangebkg');
 		} else {
 			e.target.setAttribute('src', 'pict/triangle_down.png');
 			block.classList.remove('show');
@@ -17,6 +19,12 @@ itemContainer.addEventListener('click', function (e) {
 		}
 	}
 
-
+	for (var i = 0; i < allItemContainer.length; i++) {
+		var middle = allItemContainer[i];
+		if (middle.lastElementChild.lastElementChild === e.target) {
+			//console.log('Клин на  ' + middle.lastElementChild.lastElementChild.tagName + ' № ' + i);
+		}
+	}
 
 });
+
