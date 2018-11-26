bd6aba280b8ceb44225e37f281cbe97f0d421ed8
rmtar <picpoint@yandex.ru>
2018-11-26 23:25:31 +0300

    add toggle item elements

--- dist/js/script.js
+++ dist/js/script.js
@@ -11,7 +11,7 @@ itemContainer.addEventListener('click', function (e) {
 		if (e.target.getAttribute('src') === 'pict/triangle_down.png') {
 			e.target.setAttribute('src', 'pict/triangle_up.png');
 			block.classList.add('show');
-		  item.classList.add('orangebkg');
+		 item.classList.add('orangebkg');
 		} else {
 			e.target.setAttribute('src', 'pict/triangle_down.png');
 			block.classList.remove('show');
@@ -19,10 +19,17 @@ itemContainer.addEventListener('click', function (e) {
 		}
 	}
 
+
 	for (var i = 0; i < allItemContainer.length; i++) {
 		var middle = allItemContainer[i];
 		if (middle.lastElementChild.lastElementChild === e.target) {
-			//console.log('Клин на  ' + middle.lastElementChild.lastElementChild.tagName + ' № ' + i);
+				middle.classList.add('orangebkg');
+				middle.nextElementSibling.classList.add('show');
+				middle.lastElementChild.lastElementChild.setAttribute('src', 'pict/triangle_up.png');
+		} else if (middle.lastElementChild.lastElementChild !== e.target) {
+				middle.classList.remove('orangebkg');
+				middle.nextElementSibling.classList.remove('show');
+				middle.lastElementChild.lastElementChild.setAttribute('src', 'pict/triangle_down.png');
 		}
 	}
 
